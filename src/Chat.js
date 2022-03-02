1b84f0450bef2bbe6e8c49b713dce265ad2be803
z-memory <dont@forgetmy.email>
2022-03-02 20:04:37 +0100

    remove blocking overlay

--- src/Chat.js
+++ src/Chat.js
@@ -1,34 +1,16 @@
 import { useEffect, useState } from 'react';
 
 export default function Chat() {
-  const [necessaryClick, setNecessaryClick] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setNecessaryClick(true);
-    }, 10000);
-  }, []);
   return (
-    <>
-      <div
-        className="overlay"
-        style={{
-          position: 'absolute',
-          width: '48%',
-          height: window.innerHeight / 2 - 85,
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          display: necessaryClick ? 'none' : 'block',
-        }}
-      ></div>
-      <iframe
-        id="ircChat"
-        title="Chat"
-        width="100%"
-        height={window.innerHeight / 2 - 85}
-        allowtransparency="true"
-        frameBorder="0"
-        // allow="microphone; camera; display-capture; fullscreen"
-        src={`https://kiwiirc.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunkerhub`}
-      />
-    </>
+    <iframe
+      id="ircChat"
+      title="Chat"
+      width="100%"
+      height={window.innerHeight / 2 - 85}
+      allowtransparency="true"
+      frameBorder="0"
+      // allow="microphone; camera; display-capture; fullscreen"
+      src={`https://kiwiirc.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunkerhub`}
+    />
   );
 }
