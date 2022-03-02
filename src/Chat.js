5b56e926f3db22f1295a332deb207ca5f86c6ccb
z-memory <dont@forgetmy.email>
2022-03-02 18:16:46 +0100

    first click prompt

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,32 @@
-import { useLayoutEffect } from 'react';
+import { useState } from 'react';
 
 export default function Chat() {
+  const [necessaryClick, setNecessaryClick] = useState(false);
   return (
-    
-    <iframe 
-    id="ircChat"
-    title="Chat"
-    width="100%"
-    height={window.innerHeight / 2 - 85}
-    allowtransparency="true"
-    frameBorder="0"
-    // allow="microphone; camera; display-capture; fullscreen"
-    src={`https://kiwiirc.com/client/kiwiirc.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunkerhub`} />
+    <>
+      <div
+        className="overlay"
+        style={{
+          position: 'absolute',
+          width: '50%',
+          height: window.innerHeight / 2 - 85,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          display: necessaryClick ? 'none' : 'block',
+        }}
+        onClick={() => setNecessaryClick(true)}
+      >
+        <button>Click to open chat</button>
+      </div>
+      <iframe
+        id="ircChat"
+        title="Chat"
+        width="100%"
+        height={window.innerHeight / 2 - 85}
+        allowtransparency="true"
+        frameBorder="0"
+        // allow="microphone; camera; display-capture; fullscreen"
+        src={`https://kiwiirc.com/client/kiwiirc.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunkerhub`}
+      />
+    </>
   );
 }
