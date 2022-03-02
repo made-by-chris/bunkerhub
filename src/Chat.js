c485532be5fe29afd1be0c612d611652af50cb3d
z-memory <dont@forgetmy.email>
2022-03-02 20:45:39 +0100

    rm comment

--- src/Chat.js
+++ src/Chat.js
@@ -9,7 +9,6 @@ export default function Chat() {
       height={window.innerHeight / 2 - 85}
       allowtransparency="true"
       frameBorder="0"
-      // allow="microphone; camera; display-capture; fullscreen"
       src={`https://kiwiirc.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunkerhub`}
     />
   );
