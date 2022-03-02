e8ddc45bc0faca7db8b3ab1f5a1d04c4643e1a66
Christopher Shelley <dont@forgetmy.email>
2022-03-02 17:44:41 +0100

    KIWI GANG

--- src/Chat.js
+++ src/Chat.js
@@ -2,16 +2,14 @@ import { useLayoutEffect } from 'react';
 
 export default function Chat() {
   return (
-    <iframe
-      id="ircChat"
-      title="Chat"
-      width="100%"
-      height={window.innerHeight / 2 - 85}
-      allowtransparency="true"
-      frameBorder="0"
-      src={`https://thelounge.hybridirc.com/?theme=morning&join=bunkerhub&nick=ambassador-${Math.floor(
-        Math.random() * 12303123
-      )}`}
-    />
+    
+    <iframe 
+    id="ircChat"
+    title="Chat"
+    width="100%"
+    height={window.innerHeight / 2 - 85}
+    allowtransparency="true"
+    frameBorder="0"
+    src={`https://kiwiirc.com/client/thelounge.hybridirc.com/?nick=ambassador-?&style="border:0; width:100%; height:450px;&theme=cli#bunker`} />
   );
 }
